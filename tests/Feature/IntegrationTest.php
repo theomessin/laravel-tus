17da8cdca82e3aecb081604d8421aaf715e5c8ac
Theodore <theodore@messinezis.com>
2019-10-31 18:10:51 +0000

    Fix styling issues

--- tests/Feature/IntegrationTest.php
+++ tests/Feature/IntegrationTest.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Storage;
 use Theomessin\Tus\Tests\Helpers\CanTusUpload;
 use Theomessin\Tus\Tests\TestCase;
 
-
 class IntegrationTest extends TestCase
 {
     use CanTusUpload;
